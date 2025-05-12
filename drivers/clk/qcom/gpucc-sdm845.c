28d4a3ef5fb813ecaf84e506c8ebc4b6fbdb8072
Andrey <andrey0800770@gmail.com>
2025-05-12 00:31:55 -0300

    gpu: update frequencies

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -276,12 +276,12 @@ static const struct freq_tbl ftbl_gpu_cc_gx_gfx3d_clk_src[] = {
 static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(180000000, P_CRC_DIV,  1, 0, 0),
 	F(257000000, P_CRC_DIV,  1, 0, 0),
-	F(342000000, P_CRC_DIV,  1, 0, 0),
-	F(414000000, P_CRC_DIV,  1, 0, 0),
-	F(520000000, P_CRC_DIV,  1, 0, 0),
-	F(596000000, P_CRC_DIV,  1, 0, 0),
-	F(675000000, P_CRC_DIV,  1, 0, 0),
-	F(710000000, P_CRC_DIV,  1, 0, 0),
+	F(347000000, P_CRC_DIV,  1, 0, 0),
+	F(437000000, P_CRC_DIV,  1, 0, 0),
+	F(527000000, P_CRC_DIV,  1, 0, 0),
+	F(617000000, P_CRC_DIV,  1, 0, 0),
+	F(707000000, P_CRC_DIV,  1, 0, 0),
+	F(800000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
 
@@ -630,16 +630,16 @@ static void gpu_cc_gfx_sdm845_fixup_sdm845v2(void)
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_MIN] = 180000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_LOWER] =
 		257000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_LOW] = 342000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_LOW] = 347000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_LOW_L1] =
-		414000000;
+		437000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL] =
-		520000000;
+		527000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_NOMINAL_L1] =
-		596000000;
-	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 675000000;
+		617000000;
+	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH] = 707000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
-		710000000;
+		800000000;
 }
 
 static void gpu_cc_gfx_sdm845_fixup_sdm670(void)
